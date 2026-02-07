a5b71e169c95edf8f3947f8690d5e05fdf07c32d
Davi Braz <davibraz.profissional@gmail.com>
2026-02-07 20:54:31 -0300

    feat: configurar cookies e CORS no AppModule e main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-    isGlobal: true,
-  }),
-  PrismaModule,
-],
+      isGlobal: true,
+    }),
+    JwtModule.register({ global: true }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule { }
